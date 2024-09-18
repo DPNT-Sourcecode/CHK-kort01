ab9bb021f856d55c8d5e2c54160963211bdb9b5b
sbx_user1051 <sbx_user1051@169.254.93.189>
2024-09-18 19:30:38 +0000

    Wed Sep 18 19:30:38 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,8 +4,8 @@
 # skus = unicode string
 
 def checkout(skus):
-    prices = {'A': 50, 'B':30, 'C':20, 'D':15}
-    special_offers = {'A': (3,130), 'B':(2,45)}
+    prices = {'A': 50, 'B':30, 'C':20, 'D':15, 'E':40}
+    special_offers = {'A': [(3,130),(5,200)], 'B':[(2,45)], 'E':[(2,80)]}
 
     total_cost, item_counts = 0, {}
 
@@ -19,10 +19,23 @@ def checkout(skus):
     
     for item, count in item_counts.items():
         if item in special_offers:
-            offer_count, offer_price = special_offers[item]
-            total_cost += (count // offer_count) * offer_price
-            total_cost += (count % offer_count) * prices[item]
+            for offer_count, offer_price in sorted(special_offers[item]):
+                total_cost += (count // offer_count)*offer_price
+                count %= offer_count
+            
+            total_cost += count*prices[item]
         else:
             total_cost += count*prices[item]
     
+    if 'E' in item_counts and item_counts['E'] >=2:
+        free_b_count = item_counts['E'] //2
+        if 'B' in item_counts:
+            item_counts['B'] = max(0, item_counts['B'] - free_b_count)
+            total_cost -= min(free_b_count, item_counts['B'])*prices['B']
+    
     return total_cost
+
+def test_checkout():
+    assert checkout("A") == 50
+    
+test_checkout()
