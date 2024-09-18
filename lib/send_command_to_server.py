803d42d1ddd2e46d60a268eeaf7b9deeb1d9410e
sbx_user1051 <sbx_user1051@169.254.93.189>
2024-09-18 18:56:18 +0000

    Wed Sep 18 18:56:18 UTC 2024

--- lib/send_command_to_server.py
+++ lib/send_command_to_server.py
@@ -1,6 +1,6 @@
 import sys
-from tdl.queue.queue_based_implementation_runner import QueueBasedImplementationRunnerBuilder
-from tdl.runner.challenge_session import ChallengeSession
+from tdl.queue.queue_based_implementation_runner import QueueBasedImplementationRunnerBuilder 
+from tdl.runner.challenge_session import ChallengeSession 
 from solutions.SUM import sum_solution
 from solutions.HLO import hello_solution
 from solutions.ARRS import array_sum
@@ -71,3 +71,4 @@ ChallengeSession\
     .with_config(Utils.get_config())\
     .with_action_provider(lambda: get_user_input(sys.argv[1:]))\
     .start()
+
