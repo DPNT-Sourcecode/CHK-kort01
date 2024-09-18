d05ea674d35980abf7f932820a9bd311b4cf446a
sbx_user1051 <sbx_user1051@169.254.93.189>
2024-09-18 20:19:20 +0000

    Wed Sep 18 20:19:20 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,8 +4,8 @@
 # skus = unicode string
 
 def checkout(skus):
-    prices = {'A': 50, 'B':30, 'C':20, 'D':15, 'E':40, 'F':10}
-    special_offers = {'A': [(3,130),(5,200)], 'B':[(2,45)], 'E':[(2,80)], 'F':[(3,20)]}
+    prices = {'A': 50, 'B':30, 'C':20, 'D':15, 'E':40, 'F':10, 'G':20,'H':10,'I':35,'J':60,'K':70,'L':90,'M':15,'N':40,'O':10,'P':50,'Q':30,'R':50,'S':30,'T':20,'U':40,'V':50,'W':20,'X':90,'Y':10,'Z':50}
+    special_offers = {'A': [(5,200),(3,130)], 'B':[(2,45)], 'E':[(2,80)], 'F':[(3,20)], 'H':[(10,80),(5,45)],'K':[(2,150),'N':[(3,120),'P':[(5,200),'Q':[(3,80),'R':[(3,150),'U':[(4,120),'V':[(3,130),(2,90)] }
 
     total_cost, item_counts = 0, {}
 
@@ -28,9 +28,25 @@ def checkout(skus):
         free_f_count = f_count//3
         paid_f_count = f_count - free_f_count
         item_counts['F'] = paid_f_count
+    
+    if 'N' in item_counts and item_counts['N'] >=3:
+        free_m_count = item_counts['N'] // 3
+        if 'M' in item_counts:
+            item_counts['M'] = max(0, item_counts['M'] -free_m_count)
+    
+    if 'R' in item_counts and item_counts['R'] >=3:
+        free_q_count = item_counts['R'] // 3
+        if 'Q' in item_counts:
+            item_counts['Q'] = max(0, item_counts['Q'] -free_q_count)
+    
+    if 'U' in item_counts:
+        u_count = item_counts['U']
+        free_u_count = u_count//4
+        paid_u_count = u_count - free_u_count
+        item_counts['U'] = paid_u_count
 
     for item, count in item_counts.items():
-        if item in special_offers and item != 'F':
+        if item in special_offers and item not in ['F','U','N','R']:
             for offer_count, offer_price in sorted(special_offers[item], reverse=True):
                 applicable_offers = count // offer_count
                 total_cost += applicable_offers*offer_price
@@ -60,3 +76,4 @@ def test_checkout():
     assert checkout("BBBEEEFFFF") == 195
 
 test_checkout()
+
