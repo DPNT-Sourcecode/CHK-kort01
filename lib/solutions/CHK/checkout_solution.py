36cb26856d0f3c1c64374ec9380747dc8775ab07
sbx_user1051 <sbx_user1051@169.254.93.189>
2024-09-18 20:25:33 +0000

    Wed Sep 18 20:25:33 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,7 +4,7 @@
 # skus = unicode string
 
 def checkout(skus):
-    prices = {'A': 50, 'B':30, 'C':20, 'D':15, 'E':40, 'F':10, 'G':20,'H':10,'I':35,'J':60,'K':70,'L':90,'M':15,'N':40,'O':10,'P':50,'Q':30,'R':50,'S':30,'T':20,'U':40,'V':50,'W':20,'X':90,'Y':10,'Z':50}
+    prices = {'A': 50, 'B':30, 'C':20, 'D':15, 'E':40, 'F':10, 'G':20,'H':10,'I':35,'J':60,'K':80,'L':90,'M':15,'N':40,'O':10,'P':50,'Q':30,'R':50,'S':30,'T':20,'U':40,'V':50,'W':20,'X':90,'Y':10,'Z':50}
     special_offers = {'A': [(5,200),(3,130)], 'B':[(2,45)], 'E':[(2,80)], 'F':[(3,20)], 'H':[(10,80),(5,45)],'K':[(2,150)],'N':[(3,120)],'P':[(5,200)],'Q':[(3,80)],'R':[(3,150)],'U':[(4,120)],'V':[(3,130),(2,90)] }
 
     total_cost, item_counts = 0, {}
@@ -79,6 +79,9 @@ def test_checkout():
     assert checkout("UUUU") == 120
     assert checkout("VVVV") == 180
     assert checkout("VVUUUUQQQ") == 290
+
+    assert checkout("K") == 80
+    assert checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZ") == 965
 
 test_checkout()
 
